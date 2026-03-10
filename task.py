dbe0f1373c9eb775d3de7297c64d18185c1a1b9c
Ganesh Arumugam <ganeshkarthick01@gmail.com>
2026-03-10 12:28:38 +0530

    Day 1: Add Task class

---
+++ task.py
@@ -0,0 +1,10 @@
+﻿class Task:
+    def __init__(self, title, status="pending"):
+        self.title = title
+        self.status = status
+
+    def __repr__(self):
+        return f"Task(title='{self.title}', status='{self.status}')"
+
+t = Task("Buy groceries")
+print(t)
